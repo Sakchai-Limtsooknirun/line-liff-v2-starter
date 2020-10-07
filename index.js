2dee5b0935e79863608efc15b5142524eb85463e
sakchai jay <sakchai.l@ku.th>
2020-10-08 01:28:30 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const myLiffId = process.env.MY_LIFF_ID;
+const myLiffId = process.env.MY_LIFF_ID || "1655054625-4V19vQv8";
 
 app.use(express.static('public'));
 
